d8fe11fb5c2297b92985fc445c7f43e3b71fb1ca
nick prendergast <nick_prendergast@hotmail.com>
2020-06-01 17:36:21 +0100

    tests complete

--- src/test/java/com/disaster/earthquake/EarthquakeControllerTest.java
+++ src/test/java/com/disaster/earthquake/EarthquakeControllerTest.java
@@ -1,6 +1,5 @@
-package earthquake_service;
+package com.disaster.earthquake;
 
-import com.disaster.earthquake.EarthquakeApplication;
 import com.disaster.earthquake.service.EarthquakeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,11 +37,46 @@ public class EarthquakeControllerTest {
 
     @Test
     public void standardInputTest() throws Exception {
-    float latitude = 40.730610F;
-    float longitude = -73.935242F;
+        float latitude = 40.730610F;
+        float longitude = -73.935242F;
 
         mvc.perform(get(url, latitude, longitude).accept(MediaType.APPLICATION_JSON)).andDo(print())
                 .andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void verifyOutputResultTest() throws Exception {
+        float latitude = 40.730610F;
+        float longitude = -73.935242F;
+
+        MvcResult result = mvc.perform(get(url, latitude, longitude).accept(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+
+        String[] lines = content.split("\\r?\\n");
+        assertEquals(10, lines.length);
+
+        assertTrue(isListInClosestDistanceOrder(lines));
+
+    }
+
+    private boolean isListInClosestDistanceOrder(String[] lines) {
+
+        List<Integer> distances = new ArrayList<>();
+
+        for (String line : lines) {
+            distances.add(Integer.parseInt(line.substring(line.lastIndexOf('|') + 2)));
+        }
+        assertEquals(10, distances.size());
+
+        for (int i = 0; i < distances.size() - 1; i++) {
+            if (distances.get(i) < distances.get(i + 1)) {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 
     @Test
@@ -46,8 +86,8 @@ public class EarthquakeControllerTest {
 
         mvc.perform(get(url, latitude, longitude).accept(MediaType.APPLICATION_JSON)).andDo(print())
                 .andExpect(status().isBadRequest()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                                .andExpect(status().is(400)).
-        andDo(print()).andExpect(content().string(containsString(
+                .andExpect(status().is(400)).
+                andDo(print()).andExpect(content().string(containsString(
                 "some parameters are invalid: Invalid input, The latitude must be a number between -90 and 90 and the longitude between -180 and 180.")));
     }
 
---
+++ src/test/java/com/disaster/earthquake/EarthquakeServiceTest.java
@@ -0,0 +1,13 @@
+package com.disaster.earthquake;
+
+//@SpringBootTest(classes = EarthquakeApplication.class)
+//@ExtendWith(SpringExtension.class)
+//@AutoConfigureMockMvc
+//public class EarthquakeServiceTest {
+//
+//
+//    @Autowired
+//    EarthquakeService earthquakeService;
+//
+//    @Test
+//}
